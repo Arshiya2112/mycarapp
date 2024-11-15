af7f8304be6eab6bfdb5880fd1698289f40f10ce
Arshiya2112 <153611048+Arshiya2112@users.noreply.github.com>
2024-11-15 12:52:19 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,2 @@
-export const backend_url = "http://mycarapp-myapi.vercel.app/api"
+// export const backend_url = "http://mycarapp-myapi.vercel.app/api"
+export const backend_url = "https://mycarapp-qk38.onrender.com"
